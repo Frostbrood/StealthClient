d8feb3c0b78ba493eafde625eb2dbd00faf32a7f
Braedon Watson <epikninja123@hotmail.com>
2012-08-18 16:23:49 -0600

    Session List GUI
    Added form for session selection

---
+++ src/Sessions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StealthClient
+{
+    public partial class Sessions : Form
+    {
+        public Sessions()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
